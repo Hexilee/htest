bd03198c21e9887550cb4513ab72f599d2660dac
Hexilee <hexileee@gmail.com>
2018-02-21 22:35:01 +0800

    update example, use With(*testing.T)

--- example/handler_test.go
+++ example/handler_test.go
@@ -7,18 +7,18 @@ import (
 
 func TestNameHandlerFunc(t *testing.T) {
 	client := htest.NewClient().ToFunc(NameHandler)
-	body := client.Get("").Send().OK().JSON()
+	body := client.Get("").Send().With(t).OK().JSON()
 	body.String("name", "hexi")
 }
 
 func TestNameHandler(t *testing.T) {
 	client := htest.NewClient().To(Mux)
-	body := client.Get("/name").Send().OK().JSON()
+	body := client.Get("/name").Send().With(t).OK().JSON()
 	body.String("name", "hexi")
 }
 
 func TestNameHandlerEcho(t *testing.T) {
 	client := htest.NewClient().To(server)
-	client.Get("/name").Send().OK().JSON().String("name", "hexi")
-	client.Get("/stuid").Send().OK().JSON().String("stuid", "3160100001")
+	client.Get("/name").Send().With(t).OK().JSON().String("name", "hexi")
+	client.Get("/stuid").Send().With(t).OK().JSON().String("stuid", "3160100001")
 }
